71c6028b1db8218116e5b4fad05669e3d084e8d8
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-01-25 19:12:53 +0530

    Add JumpObjects which can act like Snake or Ladder

---
+++ src/JumpObject.lua
@@ -0,0 +1,60 @@
+--[[
+    Represents an entity which can change the
+    position of the given player
+]]
+
+JumpObject = Class{}
+
+function JumpObject:init(params)
+    --[[
+        * Required params are
+        (start position)
+        startX, startY
+
+        (end position)
+        endX, endY
+    ]]
+    self.startX = params.startX
+    self.startY = params.startY
+
+    self.endX = params.endX
+    self.endY = params.endY
+
+    -- The dimension of the tiles
+    self.width = TILESIZE * 0.75
+    self.height = TILESIZE * 0.75
+end
+
+function JumpObject:collides(object)
+    --[[
+        If the object's position collides with start,
+        then true
+    ]]
+    return (object.x == self.startX and object.y == self.startY)
+end
+
+function JumpObject:move(object)
+    --[[
+        Moves the object to the end position of the jump object
+    ]]
+    object.x = self.endX
+    object.y = self.endY
+end
+
+function JumpObject:render(renderOffsetX, renderOffsetY)
+    --[[
+        For now we will render the start position with 'S'
+        and end position with 'E'
+    ]]
+    love.graphics.setFont(gFonts['small'])
+
+    love.graphics.setColor(COLORS.BLUE)
+    love.graphics.rectangle("line", (self.startX-1)*TILESIZE + TILESIZE*0.125 + renderOffsetX, (self.startY-1)*TILESIZE + TILESIZE*0.125 + renderOffsetY, self.width, self.height, 8, 8)
+    love.graphics.setColor(COLORS.DEFAULT)
+    love.graphics.printf("S", (self.startX-1)*TILESIZE + renderOffsetX, (self.startY-1)*TILESIZE + renderOffsetY + TILESIZE/2 - 8, TILESIZE, 'center')
+
+    love.graphics.setColor(COLORS.BLUE)
+    love.graphics.rectangle("line", (self.endX-1)*TILESIZE + TILESIZE*0.125 + renderOffsetX, (self.endY-1)*TILESIZE + TILESIZE*0.125 + renderOffsetY, self.width, self.height, 8, 8)
+    love.graphics.setColor(COLORS.DEFAULT)
+    love.graphics.printf("E", (self.endX-1)*TILESIZE + renderOffsetX, (self.endY-1)*TILESIZE + renderOffsetY + TILESIZE/2 - 8, TILESIZE, 'center')
+end
